52066997a841644e3f17c6c29037adbb4b6d92c5
William Asp <wille.asp@live.se>
2021-01-23 23:15:55 +0100

    added new data and reading data with pandas

--- backtrader_main.py
+++ backtrader_main.py
@@ -4,11 +4,13 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import datetime  # For datetime objects
+import pandas
 #import os.path  # To manage paths
 #import sys  # To find out the script name (in argv[0])
 
 # from backtrader.strategies.sma_crossover import MA_CrossOver as Strategy
 from strategies import DefaultStrategy as Strategy
+
 
 # Import the backtrader platform
 import backtrader as bt
@@ -40,22 +42,30 @@ if __name__ == '__main__':
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
-    datapath = 'data/orcl-1995-2014.txt'
+    datapath = 'data/extended_intraday_IBM_15min_year1month2_adjusted.csv'
 
     # Create a Data Feed
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath,
-        # Do not pass values before this date
-        fromdate=datetime.datetime(2008, 1, 1),
-        # Do not pass values after this date
-        todate=datetime.datetime(2009, 12, 31),
-        reverse=False)
+    # data = bt.feeds.YahooFinanceCSVData(
+    #     dataname=datapath,
+    #     # Do not pass values before this date
+    #     fromdate=datetime.datetime(2020, 11, 17),
+    #     # Do not pass values after this date
+    #     todate=datetime.datetime(2020, 12, 14),
+    #     reverse=False)
+
+    dataframe = pandas.read_csv(datapath,
+                                parse_dates=True,
+                                index_col=0)
+
+    data = bt.feeds.PandasData(
+        dataname=dataframe
+    )
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
 
     # Set our desired cash start
-    cerebro.broker.setcash(1000.0)
+    cerebro.broker.setcash(10000000.0)
 
     # Add a FixedSize sizer according to the stake
     cerebro.addsizer(bt.sizers.FixedSize, stake=10)
@@ -67,7 +77,7 @@ if __name__ == '__main__':
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
     # Run over everything
-    cerebro.run(maxcpus=1)
+    cerebro.run(maxcpus=1, runonce=False)
 
     # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
